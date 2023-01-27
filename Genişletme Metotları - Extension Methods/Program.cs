da29b4d66b73f18c3fd7b49b252894e871307f72
Serhan KORAL <serhan_koral@hotmail.com>
2023-01-27 10:40:04 +0300

    Proje dosyası ekle.

---
+++ Genişletme Metotları - Extension Methods/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Genişletme_Metotları___Extension_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Extention var olan parametrelere yenisi eklemek için kullanılır
+            //Class static olarak yapılmalı
+            string metin = "Serhan Koral";
+            metin.yaz();
+            metin.yaz(15);
+            
+        }
+    }
+    static class StringGenisletme
+    {
+        public static void yaz(this string metin)
+        {
+            Console.WriteLine(metin);
+        }
+        public static void yaz(this string metin,int sayi)
+        {
+            Console.WriteLine(metin+" "+sayi.ToString());
+        }        
+    }
+}
